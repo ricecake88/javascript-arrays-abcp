9b75452816b53c11514456397fc3cffb3dff9d3b
Grace Shih <vocalise@gmail.com>
2018-01-13 16:52:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-chocolateBars = ['coffee crisp', ...chocolateBars];
-
-console.log(chocolateBars);
+function addElementToBeginningOfArray(array, element) {
+  array.push(element);
+}
